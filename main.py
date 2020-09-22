a871c2ea2f78639c35d94cef5e165f5540a25850
yukiue <45696312+yukiue@users.noreply.github.com>
2020-09-23 05:21:21 +0900

    fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import cv2
+import numpy as np
 
 
 def main():
@@ -11,13 +12,22 @@ def main():
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     frames = []
-    for i in range(1, frame_count, fps):
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 1)
+    frames.append(cap.read()[1])
+
+    for i in range(2, frame_count, fps):
         cap.set(cv2.CAP_PROP_POS_FRAMES, i)
         frame = cap.read()[1]
         frames.append(frame)
-        cv2.imwrite('out/{:0=4}.jpg'.format(i), frame)
+
+        # if not np.array_equal(frames[-1], frame):
+        #     print(np.array_equal(frames[-1], frame))
+        #     frames.append(frame)
 
     cap.release()
+
+    for i, frame in enumerate(frames):
+        cv2.imwrite('out/{:0=5}.jpg'.format(i), frame)
 
 
 if __name__ == "__main__":
