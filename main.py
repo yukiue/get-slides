9d2055211a440dc800f14be4df0879cdfc5f72aa
yukiue <45696312+yukiue@users.noreply.github.com>
2020-10-08 01:50:10 +0900

    compare previous frame and current frame

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import cv2
+
+
+def has_difference(frame1, frame2, threshold=0.15):
+    # Note that frame1 and frame2 have the same shape
+
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+    height, width = frame1.shape
+
+    pixels = height * width
+    same_pixels = 0
+
+    for h in range(height):
+        for w in range(width):
+            if frame1[h][w] == frame2[h][w]:
+                same_pixels += 1
+
+    ratio = (pixels - same_pixels) / pixels
+
+    flag = True
+
+    if ratio < threshold:
+        flag = False
+
+    return flag
 
 
 def parse_args():
@@ -35,13 +62,13 @@ def main():
     for i in range(2, frame_count, fps * duration):
         cap.set(cv2.CAP_PROP_POS_FRAMES, i)
         frame = cap.read()[1]
-        frames.append(frame)
 
-        # if not np.array_equal(frames[-1], frame):
-        #     print(np.array_equal(frames[-1], frame))
-        #     frames.append(frame)
+        if has_difference(frames[-1], frame):
+            frames.append(frame)
 
     cap.release()
+
+    os.mkdir('out')
 
     for i, frame in enumerate(frames):
         cv2.imwrite('out/{:0=5}.jpg'.format(i), frame)
